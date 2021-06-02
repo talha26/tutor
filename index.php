93d6ae52c5faf285ddae8cfca02d3c6088a593a0
talha razzaq <mh.talharazzaq@gmail.com>
2021-06-02 11:58:38 +0500

    Add files via upload

--- index.php
+++ index.php
@@ -1,7 +1,17 @@
 <?php
+/**
+ * Front to the WordPress application. This file doesn't do anything, but loads
+ * wp-blog-header.php which does and tells WordPress to load the theme.
+ *
+ * @package WordPress
+ */
 
-echo "hello word application on github";
-echo "new data add";
-echo "check update";
+/**
+ * Tells WordPress to load the WordPress theme and output it.
+ *
+ * @var bool
+ */
+define( 'WP_USE_THEMES', true );
 
-?>
+/** Loads the WordPress Environment and Template */
+require __DIR__ . '/wp-blog-header.php';
